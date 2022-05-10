014bc84319dd5e142aa5babe0638d4acad54f68f
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-05-10 08:54:11 +0300

    utils - create a function to construct the baseUrl for images
    creates the baseUrl for images from imagekit

---
+++ utils.ts
@@ -0,0 +1,8 @@
+/**
+ * @TODO: @FIXME:
+ * ADD the base imagekit url to doppler and use it as an environment variable
+ */
+const imagesLoaer = (src: string): string =>
+  `https://ik.imagekit.io/3t80u6sjo/SCRATCH/Scratch/${src}`;
+
+export { imagesLoaer };

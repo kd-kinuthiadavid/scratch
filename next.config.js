1dea7c5f2515e3f10e7b03da85476264b5c0613e
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-05-10 08:50:51 +0300

    config - add an images config in next.config.js
    add config to allow us to use imagekit as our remote host for images.
    This change also includes a setting that allows us to use svgs.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-}
+  /**
+   * images config
+   * @see:https://nextjs.org/docs/messages/next-image-unconfigured-host
+   * @see: https://nextjs.org/docs/api-reference/next/image#dangerously-allow-svg
+   */
+  images: {
+    domains: ["ik.imagekit.io"],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 module.exports = nextConfig

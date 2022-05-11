89fc4a770cca38948f86f7094ed726ba738ad15d
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-05-11 06:08:25 +0300

    config | utils - replace the imagekit url with an env variable

--- utils.ts
+++ utils.ts
@@ -2,7 +2,9 @@
  * @TODO: @FIXME:
  * ADD the base imagekit url to doppler and use it as an environment variable
  */
-const imagesLoaer = (src: string): string =>
-  `https://ik.imagekit.io/3t80u6sjo/SCRATCH/Scratch/${src}`;
+
+const baseImageKitUrl = process.env.NEXT_PUBLIC_IMAGEKIT_BASE_URL;
+
+const imagesLoaer = (src: string): string => `${baseImageKitUrl}${src}`;
 
 export { imagesLoaer };

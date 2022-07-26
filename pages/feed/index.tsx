d51882db986b90e9108eb3c0efa93c0feed0d209
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-07-26 10:26:12 +0300

    feed | quality - add missing component import

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const Feed = () => {
-  return <div>This is your feed</div>;
+  return (
+    <Layout>
+      <div>This is your feed</div>
+    </Layout>
+  );
 };
 
 export default Feed;

31e26b0a235348422e46c0b66c6d75c6d0922fbd
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-08-12 18:26:02 +0300

    feat(components): create a header for the recipes section
    create header for the recipes section in the profile page

---
+++ components/Profile/ProfRecipesHeader.tsx
@@ -0,0 +1,37 @@
+import Button from "@mui/material/Button";
+import React from "react";
+import styled from "styled-components";
+import AddIcon from "@mui/icons-material/Add";
+
+const ProfRecipesHeader = () => {
+  return (
+    <Wrapper>
+      <h1>My Recipes</h1>
+      <Button
+        size="small"
+        variant="outlined"
+        startIcon={<AddIcon />}
+        style={{
+          color: "#30BE76",
+          borderColor: "#30BE76",
+          textTransform: "capitalize",
+          fontSize: "1rem",
+        }}
+      >
+        Add New
+      </Button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  h1 {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default ProfRecipesHeader;
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Layout from "../../components/Layout";
 import ProfileActions from "../../components/Profile/ProfileActions";
 import ProfileSummary from "../../components/Profile/ProfileSummary";
+import ProfRecipesHeader from "../../components/Profile/ProfRecipesHeader";
 
 const Profile = () => {
   return (
@@ -12,7 +13,9 @@ const Profile = () => {
           <ProfileSummary />
           <ProfileActions />
         </ProfileDetails>
-        <ProfileRecipes />
+        <ProfileRecipes>
+          <ProfRecipesHeader />
+        </ProfileRecipes>
       </ProfileWrapper>
     </Layout>
   );
@@ -22,18 +25,17 @@ const ProfileWrapper = styled.div`
   height: 100vh;
   background-color: #f7f8fa;
   display: grid;
-  grid-template-columns: 30% [col-start] 80% [col-start];
+  grid-template-columns: 1fr [col-start] 2fr [col-start];
   column-gap: 1.5rem;
+  padding: 0 3rem;
 `;
 
 const ProfileDetails = styled.div`
   margin-top: 2.5rem;
-  margin-left: 3rem;
 `;
 
 const ProfileRecipes = styled.div`
-  background-color: pink;
-  margin-left: 3rem;
+  margin-top: 2.5rem;
 `;
 
 export default Profile;
